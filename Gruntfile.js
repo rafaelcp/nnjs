9e02edb17460662fcca1c345c9ead9ce180e1db6
rafaelcp <kurama.youko.br@gmail.com>
2015-05-30 22:30:11 -0300

    Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+/*global module*/
+module.exports = function (grunt) {
+  'use strict';
+ 
+  var gruntConfig = {};
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  gruntConfig.jshint = {
+      options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
+          indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
+          quotmark: true, regexp: true, undef: true, unused: true, strict: true, trailing: true,
+          maxparams: 3, maxdepth: 2, maxstatements: 50},
+      all: [
+          'Gruntfile.js',
+          'src/js/**/*.js'
+      ]
+  };
+  grunt.initConfig(gruntConfig);
+  grunt.registerTask('travis', 'jshint');
+};
