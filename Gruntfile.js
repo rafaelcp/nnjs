9b333081203e2a8580ede20f8bbad93ad2aadc0b
rafaelcp <kurama.youko.br@gmail.com>
2015-05-30 22:40:21 -0300

    indentation fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,18 @@
 /*global module*/
 module.exports = function (grunt) {
-  'use strict';
- 
-  var gruntConfig = {};
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  gruntConfig.jshint = {
-      options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
-          indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
+    'use strict';
+    var gruntConfig = {};
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    gruntConfig.jshint = {
+        options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
+		  indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
           quotmark: true, regexp: true, undef: true, unused: true, strict: true, trailing: true,
           maxparams: 3, maxdepth: 2, maxstatements: 50},
-      all: [
+        all: [
           'Gruntfile.js',
           'src/js/**/*.js'
-      ]
-  };
-  grunt.initConfig(gruntConfig);
-  grunt.registerTask('travis', 'jshint');
+        ]
+    };
+    grunt.initConfig(gruntConfig);
+    grunt.registerTask('travis', 'jshint');
 };
