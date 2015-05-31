b019bdfd46f5ecd4c04f9cb62b2f396d651d7a0b
rafaelcp <kurama.youko.br@gmail.com>
2015-05-31 00:50:46 -0300

    style

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
     var gruntConfig = {};
     grunt.loadNpmTasks('grunt-contrib-jshint');
     gruntConfig.jshint = {
-        options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
+        options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true, asi: true, lastsemic: true, globalstrict: true,
           indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: false,
           quotmark: true, regexp: true, undef: false, unused: false, strict: false, trailing: true, node: true,
           maxparams: 5, maxdepth: 4, maxstatements: 70},
