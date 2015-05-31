baebf285f4416c90ac8b43c9ce0b3366677ab560
rafaelcp <kurama.youko.br@gmail.com>
2015-05-30 22:42:27 -0300

    indentation fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     gruntConfig.jshint = {
         options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
-		  indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
+          indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
           quotmark: true, regexp: true, undef: true, unused: true, strict: true, trailing: true,
           maxparams: 3, maxdepth: 2, maxstatements: 50},
         all: [
