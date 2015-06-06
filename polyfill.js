ea267eefccd946d7eb921a448dcd42f2d8f123a1
rafaelcp <kurama.youko.br@gmail.com>
2015-06-06 18:33:53 -0300

    polyfills for tanh and sign

---
+++ polyfill.js
@@ -0,0 +1,19 @@
+Math.tanh = Math.tanh || function (x) {
+  if (x === Infinity) {
+    return 1;
+  }
+  else if (x === -Infinity) {
+    return -1;
+  }
+  else {
+    var y = Math.exp(2 * x);
+    return (y - 1) / (y + 1);
+  }
+};
+Math.sign = Math.sign || function (x) {
+  x = +x; // convert to a number
+  if (x === 0 || isNaN(x)) {
+    return x;
+  }
+  return x > 0 ? 1 : -1;
+};
